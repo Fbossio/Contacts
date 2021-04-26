826fc8dc3eb20f117542c3f4bb646045781d3ca9
Felix Bossio <felixbossio@gmail.com>
2021-04-26 01:00:28 -0500

    Initial setting Blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,14 @@
+from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-form flask import Flask
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+login_manager.login_view = "auth.login"
 
 
 def create_app(config_name):
@@ -15,10 +16,19 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     from .models import User, Contact
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     # Initialize plugins
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
